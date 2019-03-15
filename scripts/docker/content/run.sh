c884145aa9e099c6a27d430fee103280af8c92db
sushilraje <suraje@microsoft.com>
2019-03-14 19:00:17 -0700

    RM integration with Keyvault

--- scripts/docker/content/run.sh
+++ scripts/docker/content/run.sh
@@ -3,6 +3,9 @@
 
 cd /app/
 
+# Running in current shell
+. set_env.sh PCS_CONFIG_WEBSERVICE_URL configWebServiceUrl PCS_SOLUTION_TYPE solutionType PCS_DEPLOYMENT_ID deploymentId PCS_SUBSCRIPTION_ID subscriptionId PCS_CLOUD_TYPE cloudType PCS_IOTHUB_NAME iotHubName PCS_SOLUTION_NAME solutionName PCS_APPINSIGHTS_INSTRUMENTATIONKEY appInsightsInstrumentationKey
+
 cd webservice
 
 dotnet Microsoft.Azure.IoTSolutions.Diagnostics.WebService.dll
